9dbf2046569464ec097867737b74e205a1f29abc
Diego Kaleb Valenzuela Carrillo <a01018992@itesm.mx>
2019-11-20 12:46:34 -0600

    Update Button Added

--- js/students.js
+++ js/students.js
@@ -1,7 +1,9 @@
 $('document').ready(function (e) {
   var element = document.getElementById("name");
+  var element2 = document.getElementById("name2");
   var nameU = localStorage.getItem('username');
   element.innerHTML = nameU;
+  element2.innerHTML = nameU;
   //insertName(nameU);
   });
 
@@ -27,5 +29,21 @@ function insertName(user){
 }*/
 
 function createTable(){
-
+  $.ajax({
+    url: 'http://35.225.18.238:5000/api/grade/student',
+    async: false,
+    method: 'POST',
+    headers: {
+      "Authorization": 'Basic ' + localStorage.getItem('credentials').toString()
+    },
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      var element = document.getElementById("name");
+      element.innerHTML = completeName;
+    },
+    error: function (data) {
+      alert("Problema con el nombre");
+    }
+  });
 }
