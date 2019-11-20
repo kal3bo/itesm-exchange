7f1ae194d9df33139ffd98c16088b1a9198d3af4
Diego Kaleb Valenzuela Carrillo <a01018992@itesm.mx>
2019-11-20 03:56:52 -0600

    Delete Node Modules & Modify Frontend

--- js/students.js
+++ js/students.js
@@ -1,2 +1,31 @@
 $('document').ready(function (e) {
+  var element = document.getElementById("name");
+  var nameU = localStorage.getItem('username');
+  element.innerHTML = nameU;
+  //insertName(nameU);
   });
+
+  /*
+function insertName(user){
+  $.ajax({
+    url: 'http://35.225.18.238:5000/api/token',
+    async: false,
+    method: 'GET',
+    headers: {
+      "Authorization": 'Basic ' + localStorage.getItem('credentials').toString()
+    },
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      var element = document.getElementById("name");
+      element.innerHTML = completeName;
+    },
+    error: function (data) {
+      alert("Problema con el nombre");
+    }
+  });
+}*/
+
+function createTable(){
+
+}
